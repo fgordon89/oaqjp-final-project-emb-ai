ba49b74d00f2a5bb5f621b7e8e5d64615f8b80eb
fgordon89 <franchesca.gordon@gmail.com>
2025-01-11 23:21:00 -0500

    Task Three finished

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,9 +1,32 @@
-import requests  # Import the requests library to handle HTTP requests
+import requests
+import json
 
-def emotion_detection (text_to_analyze):  # Define function named emotion_detection 
-    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'  # URL of the emotion detection
-    myobj = {"raw_document": {"text": text_to_analyze } }  # Create dictionary with the text to be analyzed
-    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}  # Set headers required for API request
-    response = requests.post(url, json = myobj, headers=header)  # Send a POST request to the API with text and headers
-    
-    return response.text  # Return the response text from the API
+def emotion_detector(text_to_analyse):
+    if not text_to_analyse.strip():  # Check for empty or blank input
+        return None
+
+
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    header = {
+        "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"
+    }
+    data = {"raw_document": {"text": text_to_analyse}}
+    response = requests.post(url, json=data, headers=header)
+    formatted_response = json.loads(response.text)
+
+
+    if response.status_code == 200:
+        emotion_predictions = formatted_response['emotionPredictions'][0]['emotion']
+        dominant_emotion = max(emotion_predictions.items(), key=lambda x: x[1])[0]
+
+
+        return {
+            'anger': emotion_predictions['anger'],
+            'disgust': emotion_predictions['disgust'],
+            'fear': emotion_predictions['fear'],
+            'joy': emotion_predictions['joy'],
+            'sadness': emotion_predictions['sadness'],
+            'dominant_emotion': dominant_emotion
+        }
+    else:
+        return None
