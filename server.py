f1c0536dbecce097b4c45010893fe551609b01b2
fgordon89 <franchesca.gordon@gmail.com>
2025-01-12 06:05:30 -0500

    DONE

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+"""Notes"""
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,16 +6,19 @@ app = Flask("Emotion Detection")
 
 @app.route("/emotionDetector")
 def sent_detector():
+    """Pass the text to the emotion_detector function and store the response
+       Extract the emotions from the response"""
     text_to_analyze = request.args.get('textToAnalyze')
-    dominant_emotion = emotion_detector(text_to_analyze) # Pass the text to the emotion_detector function and store the response
-    # Extract the emotions from the response
+    dominant_emotion = emotion_detector(text_to_analyze)
     if dominant_emotion is None:
         return "Invalid text! Please try again."
-    else:
-        return "The given text has been identified as {}.".format(dominant_emotion) # Return a formatted string with the dominant emotion
-    
+
+    return f"The given text has been indentifed as {dominant_emotion}"
+        # Return a formatted string with the dominant emotion
+
 @app.route("/")
 def render_index_page():
+    """Notes"""
     return render_template('index.html')
 
 if __name__ == "__main__":
