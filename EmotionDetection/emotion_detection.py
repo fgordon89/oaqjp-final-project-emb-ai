8c5db06d5696082f3b21a75a43f6f60f3c669c05
fgordon89 <franchesca.gordon@gmail.com>
2025-01-12 05:26:47 -0500

    Task 7 finished

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -2,22 +2,27 @@ import requests
 import json
 
 def emotion_detector(text_to_analyse):
-    if not text_to_analyse.strip():  # Check for empty or blank input
-        return None
-
-
+    
+    # URL for the emotion detector API
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     header = {
         "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"
     }
-    data = {"raw_document": {"text": text_to_analyse}}
-    response = requests.post(url, json=data, headers=header)
-    formatted_response = json.loads(response.text)
+    data = {"raw_document": {"text": text_to_analyse}} # Payload with text to be analyzed
+    response = requests.post(url, json=data, headers=header) # POST request to the API with the payload and headers
+    status_code = response.status_code
 
-
-    if response.status_code == 200:
+    # If the response status code is 400, set  dominant emotion to None    
+    if response.status_code == 400: 
+        formatted_response = { 'anger': None,
+                             'disgust': None,
+                             'fear': None,
+                             'joy': None,
+                             'sadness': None,
+                             'dominant_emotion': None}
+    else:
+    # If the response status code is 200, extract the dominant emotion from the response
         emotion_predictions = formatted_response['emotionPredictions'][0]['emotion']
         dominant_emotion = max(emotion_predictions.items(), key=lambda x: x[1])
-        return dominant_emotion[0]
-    else:
-        return None
+    
+    return dominant_emotion[0] # Return dominant emotion in a dictionary
